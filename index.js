57908db2c89db2028bbb5c462314dd509d66fae9
DESKTOP-OCTQC3J\tadeja <tadeja.kersic@gmail.com>
2018-05-08 21:06:39 +0200

    $port set

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ app.get('/testProge', function(req, res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("server started on Port 3000...");
 })
