d66d2e94bbd474794730520be3a44e765bc17c68
DESKTOP-OCTQC3J\tadeja <tadeja.kersic@gmail.com>
2018-05-27 23:42:18 +0200

    cors-module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var cors = require('cors');
+var methodOverride = require('method-override');
 //var expressValidator = require('expressValidator');
 
 var app = express();
@@ -17,7 +19,7 @@ app.use(helmet());
 
 //baza
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://pengui:mafiluta@ds163119.mlab.com:63119/pompenguin');
+mongoose.connect('mongodb://penguin:mafiluta@ds163119.mlab.com:63119/pompenguin');
 let db = mongoose.connection;
 mongoose.Promise = global.Promise; //da se izognemo opozorilom
 
@@ -31,25 +33,7 @@ app.use(bodyParser.urlencoded({
     extended: true
   }));
 
-// Add headers
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 //app.use(express.bodyParser({limit:'50mb'}));
 //check connection
