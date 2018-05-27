9281145e0e78ee8b4d6c5bedea6375fa7bb6d4ef
DESKTOP-OCTQC3J\tadeja <tadeja.kersic@gmail.com>
2018-05-28 00:30:52 +0200

    setHeader

--- index.js
+++ index.js
@@ -16,13 +16,17 @@ app.use(helmet());
 //menjaj console z debug
 
 //set header
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-});
+});*/
+app.all('/*', function(req, res, next) { 
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
+    next(); });
 app.use(express.methodOverride());
 app.use(cors());
 
