f129ef9754047725eb01d530943147de556e4959
DESKTOP-OCTQC3J\tadeja <tadeja.kersic@gmail.com>
2018-05-28 00:32:34 +0200

    setHeader

--- index.js
+++ index.js
@@ -24,9 +24,12 @@ app.use(helmet());
     next();
 });*/
 app.all('/*', function(req, res, next) { 
-    res.header("Access-Control-Allow-Origin", "*"); 
-    res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
-    next(); });
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(express.methodOverride());
 app.use(cors());
 
