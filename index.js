1697be6aa834d6c77fb88673a545f5af2d49b444
DESKTOP-OCTQC3J\tadeja <tadeja.kersic@gmail.com>
2018-05-27 23:46:29 +0200

    Header settings

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ var helmet = require('helmet');
 app.use(helmet());
 //menjaj console z debug
 
-
+//set header
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 //baza
 var mongoose = require('mongoose');
